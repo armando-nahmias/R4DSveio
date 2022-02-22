e9d706be8138a338e9ab7856ffbd2c5fc357a9cd
Armando Nahmias <armando@informartizar.com>
2022-02-22 13:12:32 -0400

    Módulo 4

--- m2ex3.R
+++ m2ex3.R
@@ -11,20 +11,28 @@
 valores <- c(54, 0, 17, 94, 12.5, 2, 0.9, 15)
 
 ## m2.ex3.q1.1. Some o objeto acima com os valores 5, 6.
-
-
+soma_1 <- valores + c(5,6)
 
 ## m2.ex3.q1.2. Some o objeto acima com os valores 5, 6, 7.
+soma_2 <- valores + c(5,6,7)
 
-## m2.ex3.q2. Construa um único objeto com as letras: A, B, e C, repetidas cada uma 15, 12, e 8 vezes, respectivamente.
+## m2.ex3.q2. Construa um único objeto com as letras: A, B, e C,
+## repetidas cada uma 15, 12, e 8 vezes, respectivamente.
+objeto <- rep(c("A", "B", "C"), times = c(15,12,8))
 
 ## m2.ex3.q2.1. Mostre na tela, em forma de verdadeiro ou falso, onde estão as letras B nesse objeto.
+objeto == "B"
 
 ## m2.ex3.q2.2. Veja a página de ajuda da função sum() e descubra como fazer para contar o número de letras B neste vetor (usando sum()).
+soma = sum(objeto == "B")
 
-## m2.ex3.q3. Crie um objeto com 100 valores aleatórios de uma distribuição uniforme U(0,1). Conte quantas vezes aparecem valores maiores ou iguais a 0,5.
+## m2.ex3.q3. Crie um objeto com 100 valores aleatórios de uma distribuição uniforme U(0,1).
+## Conte quantas vezes aparecem valores maiores ou iguais a 0,5.
+valores <- runif(100, 0, 1)
+quant_valores <- sum(valores < 0.5)
 
 ## m2.ex3.q4. Calcule as 50 primeiras potências de 2, ou seja, 2,22,23,…,250.
+
 
 ## m2.ex3.q4.1. Calcule o quadrado dos números inteiros de 1 a 50, ou seja, 12,22,32,…,502.
 

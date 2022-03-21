049de533dafa23bcf049cf82a3a15cc409f3590a
Armando Nahmias <armando@informartizar.com>
2022-03-21 18:21:45 -0400

    Atividade

---
+++ Modulo 06/Atividade.R
@@ -0,0 +1,10 @@
+##======================================================================
+## Resolução das Atividades
+## Módulo 6
+## Atividade
+## Docente: Fernando Mayer
+## Discente: Armando Nahmias
+##======================================================================
+
+##----------------------------------------------------------------------
+## Responda as questões abaixo sobre indexação e seleção condicional.
